d8f6d3ffcb60cd780d11a90f161938a10372c193
UglyGhoulChrist <UglyGhoulChrist@gmail.com>
2023-02-09 22:43:47 +0300

    <Menu />

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,22 @@ import HomePage from "./components/HomePage";
 import DriftPage from "./components/DriftPage";
 import TimeAttackPage from "./components/TimeAttackPage";
 import ForzaPage from "./components/ForzaPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Menu from "./components/Menu";
 
 function App() {
   return (
-    <div className="page">
-      <HomePage />
-      <DriftPage />
-      <TimeAttackPage />
-      <ForzaPage />
-    </div>
+    <BrowserRouter>
+      <div className="page">
+        <Menu />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="drift" element={<DriftPage />} />
+          <Route path="timeattack" element={<TimeAttackPage />} />
+          <Route path="forza" element={<ForzaPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Menu() {
-  return <div>Menu</div>;
+  return (
+    <nav className="menu">
+      <NavLink to="/" className="menu__item">
+        Главная
+      </NavLink>
+      <NavLink to="drift" className="menu__item">
+        Дрифт-такси
+      </NavLink>
+      <NavLink to="timeattack" className="menu__item">
+        Time Attack
+      </NavLink>
+      <NavLink to="forza" className="menu__item">
+        Forza Karting
+      </NavLink>
+    </nav>
+  );
 }
 
 export default Menu;
