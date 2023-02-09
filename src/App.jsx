62f2ea53232164b84fb37126ff8da905be4af8c0
UglyGhoulChrist <UglyGhoulChrist@gmail.com>
2023-02-09 22:18:40 +0300

    created elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import HomePage from "./components/HomePage";
+import DriftPage from "./components/DriftPage";
+import TimeAttackPage from "./components/TimeAttackPage";
+import ForzaPage from "./components/ForzaPage";
 
 function App() {
-  return <div className="App">APP</div>;
+  return (
+    <div className="page">
+      <HomePage />
+      <DriftPage />
+      <TimeAttackPage />
+      <ForzaPage />
+    </div>
+  );
 }
 
 export default App;
